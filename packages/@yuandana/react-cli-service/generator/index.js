0ba75ea720e99ffce6226bfcd5cb2e232083a79e
zrain <zrainma@sina.com>
2019-10-09 14:28:57 +0800

    #

--- packages/@yuandana/react-cli-service/generator/index.js
+++ packages/@yuandana/react-cli-service/generator/index.js
@@ -1,3 +1,14 @@
 module.exports = (api, cliOptions) => {
     api.render('./template');
+
+    api.extendPackage({
+        scripts: {
+            start: 'react-cli-service start',
+            build: 'react-cli-service build'
+        },
+        dependencies: {
+            react: '^16.10.2',
+            'react-dom': '^16.10.2'
+        }
+    });
 };
---
+++ packages/@yuandana/react-cli-service/generator/template/README.md
@@ -0,0 +1,5 @@
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn start`
